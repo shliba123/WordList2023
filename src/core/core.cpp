7a09ccd3a6a5ec93c77812c7e8a3addce1e1a40f
shliba123 <shliba0828@163.com>
2023-03-07 21:42:45 +0800

    初始化仓库，简单设置架构

---
+++ src/core/core.cpp
@@ -0,0 +1,5 @@
+//
+// Created by shliba on 2023/3/7.
+//
+
+#include "core.h"
---
+++ src/core/core.h
@@ -0,0 +1,8 @@
+//
+// Created by shliba on 2023/3/7.
+//
+
+#ifndef WORDLIST2023_CORE_H
+#define WORDLIST2023_CORE_H
+
+#endif // WORDLIST2023_CORE_H
