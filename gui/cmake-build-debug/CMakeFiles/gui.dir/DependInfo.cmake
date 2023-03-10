164f8025d2b4366be2888a2f6b2a3426ec277d6a
shliba123 <shliba0828@163.com>
2023-03-10 20:22:06 +0800

    增加单元测试

---
+++ gui/cmake-build-debug/CMakeFiles/gui.dir/DependInfo.cmake
@@ -0,0 +1,45 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/shliba/CLionProjects/WordList2023/gui/cmake-build-debug/gui_autogen/mocs_compilation.cpp" "/Users/shliba/CLionProjects/WordList2023/gui/cmake-build-debug/CMakeFiles/gui.dir/gui_autogen/mocs_compilation.cpp.o"
+  "/Users/shliba/CLionProjects/WordList2023/gui/main.cpp" "/Users/shliba/CLionProjects/WordList2023/gui/cmake-build-debug/CMakeFiles/gui.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "gui_autogen/include"
+  "/opt/homebrew/lib/QtCore.framework"
+  "/opt/homebrew/lib/QtCore.framework/Headers"
+  "/opt/homebrew/share/qt/mkspecs/macx-clang"
+  "/opt/homebrew/include"
+  "/opt/homebrew/lib/QtGui.framework"
+  "/opt/homebrew/lib/QtGui.framework/Headers"
+  "/opt/homebrew/lib/QtWidgets.framework"
+  "/opt/homebrew/lib/QtWidgets.framework/Headers"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,12 @@
 // Created by shliba on 2023/3/7.
 //
 #include "./control/control.h"
-
+#include "gtest/gtest.h"
 using namespace std;
 
 int main(int argc, char *argv[])
 {
     control(argc, argv);
-
-    return 0;
+    ::testing::InitGoogleTest();
+    return RUN_ALL_TESTS();
 }
