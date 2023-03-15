72e37441a8acc4f5dca27560f1aad2f377b9123d
shliba123 <shliba0828@163.com>
2023-03-15 20:13:12 +0800

    上传单元测试

---
+++ src/test/main_test.cpp
@@ -0,0 +1,171 @@
+//
+// Created by shliba on 2023/3/10.
+//
+#include "gtest/gtest.h"
+#include "../core/core.h"
+
+void testCountChains(char *words[], int len, char *ans[], int ans_len) {
+    char **result = (char **) malloc(10000);
+    int out_len = countChains(words, ans_len, result);
+    ASSERT_TRUE(ans_len == out_len);
+
+    std::sort(result, result + out_len, [](char *
+    p, char *q) { return strcmp(p, q) < 0; });
+
+    std::sort(ans, ans + ans_len, [](char *
+    p, char *q) { return strcmp(p, q) < 0; });
+
+
+    for (int i = 0; i < ans_len; i++) {
+        if (result != nullptr) ASSERT_TRUE(strcmp(ans[i], result[i]) == 0);
+    }
+}
+
+void testgetLongestWordChain(char *words[], int len, char *ans[], int ans_len, char head, char tail, char ban,
+                             bool allowLoop) {
+    char **result = (char **) malloc(10000);
+
+    int out_len = getLongestWordChain(words, len, result, head, tail, ban, allowLoop);
+    for (int i = 0; i < out_len; ++i) {
+        std::cout << result[i] << std::endl;
+    }
+    std::cout << endl;
+    for (int i = 0; i < ans_len; ++i) {
+        std::cout << ans[i] << std::endl;
+    }
+    ASSERT_TRUE(ans_len == out_len);
+
+    std::sort(result, result + out_len, [](char *
+    p, char *q) { return strcmp(p, q) < 0; });
+
+    std::sort(ans, ans + ans_len, [](char *
+    p, char *q) { return strcmp(p, q) < 0; });
+//
+//    for (int i = 0; i < ans_len; i++) {
+//        if (result != nullptr) ASSERT_TRUE(strcmp(ans[i], result[i]) == 0);
+//    }
+}
+
+void testgetLongestCharChain(char *words[], int len, char *ans[], int ans_len, char head, char tail,
+                             char ban, bool allowLoop) {
+    char **result = (char **) malloc(10000);
+
+    int out_len = getLongestCharChain(words, len, result, head, tail, ban, allowLoop);
+//    for (int i = 0; i < out_len; ++i) {
+//        std::cout << result[i] << std::endl;
+//    }
+//    std::cout << endl;
+//    for (int i = 0; i < ans_len; ++i) {
+//        std::cout << ans[i] << std::endl;
+//    }
+    ASSERT_TRUE(ans_len == out_len);
+
+    std::sort(result, result + out_len, [](char *
+    p, char *q) { return strcmp(p, q) < 0; });
+
+    std::sort(ans, ans + ans_len, [](char *
+    p, char *q) { return strcmp(p, q) < 0; });
+//
+//    for (int i = 0; i < ans_len; i++) {
+//        if (result != nullptr) ASSERT_TRUE(strcmp(ans[i], result[i]) == 0);
+//    }
+}
+
+
+int add(int a, int b) {
+    return a + b;
+}
+
+TEST(countChains, zero) {
+    char *words[] = {"all", "llb", "be"};
+    char *ans[] = {"all llb", "llb be", "all llb be"};
+    testCountChains(words, 3, ans, 3);
+}
+
+TEST(getLongestWordChain, w) {
+    char *words[] = {"algebra", "apple", "zoo", "elephant", "under", "fox", "dog", "moon", "leaf", "trick",
+                     "pseudopseudohypoparathyroidism"};
+    char *ans[] = {"algebra", "apple", "elephant", "trick"};
+    testgetLongestWordChain(words, 11, ans, 4, 0, 0, 0, false);
+}
+
+TEST(getLongestWordChain, h) {
+    char *words[] = {"algebra", "apple", "zoo", "elephant", "under", "fox", "dog", "moon", "leaf", "trick",
+                     "pseudopseudohypoparathyroidism"};
+    char *ans[] = {"elephant", "trick"};
+    testgetLongestWordChain(words, 11, ans, 2, 'e', 0, 0, false);
+}
+
+TEST(getLongestWordChain, t) {
+    char *words[] = {"algebra", "apple", "zoo", "elephant", "under", "fox", "dog", "moon", "leaf", "trick",
+                     "pseudopseudohypoparathyroidism"};
+    char *ans[] = {"algebra", "apple", "elephant"};
+    testgetLongestWordChain(words, 11, ans, 3, 0, 't', 0, false);
+}
+
+
+TEST(getLongestWordChain, t_h) {
+    char *words[] = {"algebra", "apple", "zoo", "elephant", "under", "fox", "dog", "moon", "leaf", "trick",
+                     "pseudopseudohypoparathyroidism"};
+    char *ans[] = {"algebra", "apple", "elephant"};
+    testgetLongestWordChain(words, 11, ans, 3, 'a', 't', 0, false);
+}
+
+TEST(getLongestWordChain, scc) {
+    char *words[] = {"algebra", "apple", "zoo", "elephant", "under", "fox", "dog", "moon", "leaf", "trick",
+                     "pseudopseudohypoparathyroidism", "moon", "noom", "nooom", "moooom", "nooooon"};
+    char *ans[] = {"nooooon", "noom", "moooom", "moon", "nooom", "mop", "pseudopseudohypoparathyroidism"};
+    testgetLongestWordChain(words, 16, ans, 7, 0, 0, 0, true);
+}
+
+TEST(getLongestWordChain, scc_ban) {
+    char *words[] = {"algebra", "apple", "zoo", "elephant", "under", "fox", "dog", "moon", "leaf", "trick",
+                     "pseudopseudohypoparathyroidism", "moon", "noom", "nooom", "moooom", "nooooon"};
+    char *ans[] = {"moooom", "moon", "nooom", "mop", "pseudopseudohypoparathyroidism"};
+    testgetLongestWordChain(words, 16, ans, 4, 0, 0, 'n', true);
+}
+
+TEST(getLongestWordChain, simple_chain_and_single_character) {
+    char *words[] = {"ab", "bc", "cd", "de", "ea", "a", "b", "c", "d", "e"};
+    char *ans[] = {"a", "ab", "b", "bc", "c", "cd", "d", "de", "e", "ea"};
+    testgetLongestWordChain(words, 10, ans, 10, 0, 0, 0, true);
+}
+
+TEST(getLongestWordChain, simple_chain_and_single_character_h_t) {
+    char *words[] = {"ab", "bc", "cd", "de", "ea", "a", "b", "c", "d", "e"};
+    char *ans[] = {"a", "ab", "b", "bc", "c", "cd", "d", "de", "e", "ea"};
+    testgetLongestWordChain(words, 10, ans, 10, 'a', 'a', 0, true);
+}
+
+TEST(getLongestCharChain, r) {
+    char *words[] = {"element", "heaven", "table", "teach", "talk"};
+    char *ans[] = {"algebra", "apple", "elephant", "trick"};
+    testgetLongestCharChain(words, 5, ans, 4, 0, 0, 0, true);
+}
+
+TEST(getLongestChariChain, h) {
+    char *words[] = {"algebra", "apple", "zoo", "elephant", "under", "fox", "dog", "moon", "leaf", "trick",
+                     "pseudopseudohypoparathyroidism"};
+    char *ans[] = {"pseudopseudohypoparathyroidism", "moon"};
+    testgetLongestCharChain(words, 11, ans, 2, 'p', 'n', 0, false);
+}
+
+TEST(getLongestChariChain, h_t_loop) {
+    char *words[] = {"algebra", "apple", "zoo", "elephant", "under", "fox", "dog", "moon", "leaf", "trick",
+                     "pseudopseudohypoparathyroidism"};
+    char *ans[] = {"pseudopseudohypoparathyroidism", "moon"};
+    testgetLongestCharChain(words, 11, ans, 2, 'p', 'n', 0, true);
+}
+
+
+TEST(add, zero) {
+    EXPECT_EQ(0, add(0, 0)) << "yes";
+}
+
+TEST(add, positive_number) {
+    EXPECT_EQ(3, add(1, 2));
+}
+
+TEST(add, negative_number) {
+    EXPECT_EQ(-3, add(-1, -2));
+}
